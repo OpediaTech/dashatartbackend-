ae9fee6dfbbd5f5079e11592283a92fcb19d7be6
opppdia <hannan.opppdia@gmail.com>
2022-10-08 17:17:05 +0600

    all sent to github on 8-10-2022

---
+++ controller/productController.js
@@ -0,0 +1,219 @@
+const Product = require("../models/Product");
+
+const addProduct = async (req, res) => {
+  try {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.status(200).send({
+      message: "Product Added Successfully!",
+    });
+  } catch (err) {
+    res.status(500).send({
+      message: err.message,
+    });
+  }
+};
+
+const addAllProducts = async (req, res) => {
+  try {
+    await Product.deleteMany();
+    await Product.insertMany(req.body);
+    res.status(200).send({
+      message: "Product Added successfully!",
+    });
+  } catch (err) {
+    res.status(500).send({
+      message: err.message,
+    });
+  }
+};
+
+const getShowingProducts = async (req, res) => {
+  try {
+    const products = await Product.find({ status: "Show" }).sort({ _id: -1 });
+    res.send(products);
+  } catch (err) {
+    res.status(500).send({
+      message: err.message,
+    });
+  }
+};
+
+const getDiscountedProducts = async (req, res) => {
+  try {
+    const products = await Product.find({ discount: { $gt: 5 } }).sort({
+      _id: -1,
+    });
+    res.send(products);
+  } catch (err) {
+    res.status(500).send({
+      message: err.message,
+    });
+  }
+};
+
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find({}).sort({ _id: -1 });
+    res.send(products);
+  } catch (err) {
+    res.status(500).send({
+      message: err.message,
+    });
+  }
+};
+
+const getStockOutProducts = async (req, res) => {
+  try {
+    const products = await Product.find({ quantity: { $lt: 1 } }).sort({
+      _id: -1,
+    });
+
+    res.send(products);
+  } catch (err) {
+    res.status(500).send({
+      message: err.message,
+    });
+  }
+};
+
+const getProductBySlug = async (req, res) => {
+  try {
+    const product = await Product.find({ parent: req.params.slug });
+    res.send([product]);
+  } catch (err) {
+    res.status(500).send({
+      message: `Slug problem, ${err.message}`,
+    });
+  }
+};
+
+const getProductByParent = async (req, res) => {
+  console.log("Params for parent: ", req.params.parent);
+  try {
+    const product = await Product.findOne({ parent: "Custome" });
+
+    res.status(200).send({ "all prpducts": "data" });
+  } catch (err) {
+    res.status(500).send({
+      message: `Slug problem, ${err.message}`,
+    });
+  }
+};
+
+const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.send(product);
+  } catch (err) {
+    res.status(500).send({
+      message: err.message,
+    });
+  }
+};
+
+const updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      product.sku = req.body.sku;
+      product.title = req.body.title;
+      product.slug = req.body.slug;
+      product.description = req.body.description;
+      product.parent = req.body.parent;
+      product.children = req.body.children;
+      product.type = req.body.type;
+      product.unit = req.body.unit;
+      product.quantity = req.body.quantity;
+      product.originalPrice = req.body.originalPrice;
+      product.price = req.body.price;
+      product.discount = req.body.discount;
+      product.image = req.body.image;
+      product.tag = req.body.tag;
+      await product.save();
+      res.send({ data: product, message: "Product updated successfully!" });
+    }
+    // handleProductStock(product);
+  } catch (err) {
+    res.status(404).send(err.message);
+  }
+};
+
+const updateStatus = (req, res) => {
+  const newStatus = req.body.status;
+  Product.updateOne(
+    { _id: req.params.id },
+    {
+      $set: {
+        status: newStatus,
+      },
+    },
+    (err) => {
+      if (err) {
+        res.status(500).send({
+          message: err.message,
+        });
+      } else {
+        res.status(200).send({
+          message: `Product ${newStatus} Successfully!`,
+        });
+      }
+    }
+  );
+};
+
+const deleteProduct = (req, res) => {
+  Product.deleteOne({ _id: req.params.id }, (err) => {
+    if (err) {
+      res.status(500).send({
+        message: err.message,
+      });
+    } else {
+      res.status(200).send({
+        message: "Product Deleted Successfully!",
+      });
+    }
+  });
+};
+
+// @desc Fetch a single product
+// @route GET /api/products/search/:search
+// @access Public.
+const getSearchProducts = async (req, res) => {
+  const searchKey = req.params.searchtitle.toLowerCase();
+  console.log(searchKey);
+  const product = await Product.find({});
+
+  // var text = "Airpods Wireless Bluetooth Headphones";
+  // var resu = text.includes("Airpods");
+  // console.log(resu);
+  // const allSearchedProducts = product.filter((i) => console.log(typeof i.name));
+  const allSearchedProducts = product.filter((i) => {
+    const mainname = i.title.toLowerCase();
+    return mainname.includes(searchKey) && i;
+  });
+  console.log(allSearchedProducts);
+
+  if (product) {
+    res.status(200).json({ total: product.length, allSearchedProducts });
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+};
+
+module.exports = {
+  addProduct,
+  addAllProducts,
+  getAllProducts,
+  getShowingProducts,
+  getDiscountedProducts,
+  getStockOutProducts,
+  getProductById,
+  getProductBySlug,
+  updateProduct,
+  updateStatus,
+  deleteProduct,
+  getProductByParent,
+  getSearchProducts,
+};
